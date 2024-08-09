35ab5a9179c20da45e2c46009c2f1161b2de3b52
hadeya3ik <133555427+hadeya3ik@users.noreply.github.com>
2024-08-08 20:47:00 -0400

    adding type safety to api index

--- api/index.tsx
+++ api/index.tsx
@@ -1,12 +1,26 @@
-import axios from 'axios';
+
+import axios, { AxiosError } from 'axios';
+
+interface DeviceProps {
+  sku : string,
+  device : string,
+  value ?: number, 
+  r ?: number, 
+  g ?: number, 
+  b ?: number, 
+}
 
 export async function getDevices() {
   try {
     const response = await axios.get("/api/devices");
     return response.data;
-  } catch (error) {
-    if (error.response) {
-      alert(`Error: ${error.response.status} - ${error.response.data.error}`);
+  } catch (error: unknown) {
+    if (axios.isAxiosError(error)) {
+      if (error.response) {
+        alert(`Error: ${error.response.status} - ${error.response.data.error}`);
+      } else {
+        alert('Something went wrong with the request');
+      }
     } else {
       alert('uh oh');
     }
@@ -14,10 +28,7 @@ export async function getDevices() {
   }
 }
 
-
-
-
-export async function setDeviceColor(sku, device, r, g, b) {
+export async function setDeviceColor(sku : string, device : string, r : number, g : number, b : number) {
   if (device == "n/a" && sku == "n/a") {
     return
   }
@@ -28,17 +39,19 @@ export async function setDeviceColor(sku, device, r, g, b) {
       value: ((r & 0xFF) << 16) | ((g & 0xFF) << 8) | ((b & 0xFF) << 0)
     });
     console.log('Response:', req.data);
-  } catch (error) {
-    if (error.response) {
-      alert(`Error: ${error.response.status} - ${error.response.data}`);
-    } else {
-      alert('Error toggling the light');
+  } catch (error: unknown) {
+    if (axios.isAxiosError(error)) {
+      if (error.response) {
+        alert(`Error: ${error.response.status} - ${error.response.data}`);
+      } else {
+        alert('Error toggling the light');
+      }
     }
     console.error(error);
   }
 }
 
-  export async function getDeviceState(sku, device, value) {
+  export async function getDeviceState(sku : string, device : string, value : number) {
     try {
       const req = await fetch('/api/state', {
         method: 'POST',
@@ -53,16 +66,18 @@ export async function setDeviceColor(sku, device, r, g, b) {
       if (!req.ok) {
         throw new Error(data.error || 'Error toggling the light');
       }
-  
-      // console.log('Response:', data);
+
       return data.payload.capabilities;
     } catch (error) {
-      alert(error.message);
-      console.error(error);
+      if (axios.isAxiosError(error)) {
+        alert(error.message);
+        console.error(error);
+      }
+      
     }
   }
 
-export async function setDeviceLight(sku, device, value) {
+export async function setDeviceLight(sku : string, device : string, value : number) {
   
   if (device == "n/a" && sku == "n/a") {
     return
@@ -84,13 +99,15 @@ export async function setDeviceLight(sku, device, value) {
     }
 
     console.log('Response:', data);
-  } catch (error) {
-    alert(error.message);
-    console.error(error);
+  } catch (error : unknown) {
+    if (axios.isAxiosError(error)) {
+      alert(error.message);
+      console.error(error);
+    }
   }
 }
 
-export async function setDeviceBrightness(sku, device, value) {
+export async function setDeviceBrightness(sku : string, device : string, value : number) {
   if (device == "n/a" && sku == "n/a") {
     return
   }
@@ -101,17 +118,19 @@ export async function setDeviceBrightness(sku, device, value) {
       value
     });
     console.log('Response:', req.data);
-  } catch (error) {
-    if (error.response) {
-      alert(`Error: ${error.response.status} - ${error.response.data}`);
-    } else {
-      alert('Error toggling the light');
+  } catch (error : unknown) {
+    if (axios.isAxiosError(error)) { 
+      if (error.response) {
+        alert(`Error: ${error.response.status} - ${error.response.data}`);
+      } else {
+        alert('Error toggling the light');
+      }
     }
     console.error(error);
   }
 }
 
-export async function setDeviceTemperature(sku, device, value) {
+export async function setDeviceTemperature(sku : string, device : string, value : number) {
   if (device == "n/a" && sku == "n/a") {
     return
   }
@@ -122,11 +141,13 @@ export async function setDeviceTemperature(sku, device, value) {
       value
     });
     console.log('Response:', req.data);
-  } catch (error) {
+  } catch (error : unknown) {
+    if (axios.isAxiosError(error)) { 
     if (error.response) {
       alert(`Error: ${error.response.status} - ${error.response.data}`);
     } else {
       alert('Error toggling the light');
+    }
     }
     console.error(error);
   }
